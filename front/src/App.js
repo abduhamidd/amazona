9bfa2f502ac0a3b3c78ae7821a75722866711ed0
abduhamidd <latipovabduhamid@gmail.com>
2020-12-19 13:07:38 +0500

    Register function implemented

--- front/src/App.js
+++ front/src/App.js
@@ -7,6 +7,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import SigninScreen from './screens/SigninScreen';
 import { signout } from './actions/userActions';
+import RegisterScreen from './screens/RegisterScreen';
 
 function App() {
   const cart = useSelector((state) => state.cart);
@@ -56,6 +57,7 @@ function App() {
           <Route path='/cart/:id?' component={CartScreen}></Route>
           <Route path='/product/:id' component={ProductScreen}></Route>
           <Route path='/' component={HomeScreen} exact></Route>
+          <Route path='/register' component={RegisterScreen}></Route>
         </main>
         <footer className='row center'>All right reserved</footer>
       </div>
